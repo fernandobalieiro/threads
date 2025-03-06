357e4f8a7b5873257c4a04155cbd450d69b32020
Fernando Balieiro <fernandobalieiro@gmail.com>
2025-03-06 14:59:49 +0000

    Callable.

--- src/main/java/com/training/quarkus/Threads.java
+++ src/main/java/com/training/quarkus/Threads.java
@@ -1,23 +1,33 @@
 package com.training.quarkus;
 
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.concurrent.Callable;
+import java.util.stream.IntStream;
 
 public class Threads {
 
     public static void main(String[] args) {
-        final var runnable = new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(5000);
-                    System.out.println("Hello World!");
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        };
 
-        runnable.run();
+        //final var result = new IntegerCalculation();
+        final var list = new ArrayList<Integer>();
+
+        for (int i = 0; i < 10; i++) {
+            final var result2 = new IntegerCalculation();
+            list.add(result2.call());
+        }
+
+        System.out.println("Result: " + list);
+    }
+
+    final static class IntegerCalculation implements Callable<Integer> {
+        @Override
+        public Integer call() {
+            final var random = new Random();
+            System.out.println(Thread.currentThread().getName());
+            return random.nextInt();
+        }
+
     }
 }
+
