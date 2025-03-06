42b8681f4b702ddb98dc85d1307e184d9ca987a8
Fernando Balieiro <fernandobalieiro@gmail.com>
2025-03-06 15:12:03 +0000

    Callable with executors.

--- src/main/java/com/training/quarkus/Threads.java
+++ src/main/java/com/training/quarkus/Threads.java
@@ -3,28 +3,51 @@ package com.training.quarkus;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.concurrent.Callable;
-import java.util.stream.IntStream;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import com.training.quarkus.repository.CallerRepository;
 
 public class Threads {
 
-    public static void main(String[] args) {
+    private static final ExecutorService executor = Executors.newFixedThreadPool(10);
 
-        //final var result = new IntegerCalculation();
-        final var list = new ArrayList<Integer>();
+    public static void main(String[] args) throws InterruptedException {
+
+        final var list = new ArrayList<Future<Integer>>();
 
         for (int i = 0; i < 10; i++) {
-            final var result2 = new IntegerCalculation();
-            list.add(result2.call());
+            final var calculation = new IntegerCalculation(CallerRepository.getInstance());
+            list.add(executor.submit(calculation));
         }
 
-        System.out.println("Result: " + list);
+        executor.awaitTermination(1000, java.util.concurrent.TimeUnit.MILLISECONDS);
+
+        list.forEach(e -> {
+            try {
+                System.out.println("Result: " + e.get());
+            } catch (InterruptedException ex) {
+                throw new RuntimeException(ex);
+            } catch (ExecutionException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
     }
 
     final static class IntegerCalculation implements Callable<Integer> {
+
+        final CallerRepository callerRepository;
+
+        public IntegerCalculation(final CallerRepository callerRepository) {
+            this.callerRepository = callerRepository;
+        }
+
         @Override
         public Integer call() {
             final var random = new Random();
-            System.out.println(Thread.currentThread().getName());
+            System.out.println("Inside Thread: " + Thread.currentThread().getName());
             return random.nextInt();
         }
 
